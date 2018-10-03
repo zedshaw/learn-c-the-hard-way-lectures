cbc5def796a5a3fd785f50006f1f889a8c6fc896
xWormx <42252397+xWormx@users.noreply.github.com>
2018-10-03 21:25:49 +0200

    Change ex3.c line 8
    In the book the formatting refers to age and not argv. 
    i got confused, is there a reason?
    
    best regards
    guy from sweden

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     int age = 100;
     int height = 72;
 
-    printf("I am %d years old.\n", argv);
+    printf("I am %d years old.\n", age);
     printf("I am %d inches tall.\n", height);
 
     return 0;
