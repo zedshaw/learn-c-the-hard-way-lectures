a66127660e47839c6cc9dc2586d8f4c3fef8d440
Chris Fraser <mrfunnel@gmail.com>
2020-02-13 21:20:15 +0200

    Fixed why Y in Why was falling through the cracks

--- ex10/ex10.c
+++ ex10/ex10.c
@@ -43,8 +43,8 @@ int main(int argc, char *argv[])
                 if (i > 2) {
                     // it's only sometimes Y
                     printf("%d: 'Y'\n", i);
+                    break;
                 }
-                break;
 
             default:
                 printf("%d: %c is not a vowel\n", i, letter);
