81b7626d1cd24e6da9625bbb59730d8e6818c91c
Samuel Olembo <samuel.olembo@twiga.com>
2021-10-21 06:56:39 +0300

    Get struct working

--- ex22/ex22.c
+++ ex22/ex22.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "ex22.h"
 #include "dbg.h"
+#include <stdint.h>
 
 int get_age(struct State *state)
 {
@@ -9,20 +10,29 @@ int get_age(struct State *state)
 
 void set_age(struct State *state, int age)
 {
-   state->the_age = age; 
+    state->the_age = age;
+}
+
+int get_size(struct State *state)
+{
+    return state->the_size;
+}
+
+void set_size(struct State *state, int the_size)
+{
+    state->the_size = the_size;
 }
 
 double update_ratio(double new_ratio)
 {
     static double ratio = 1.0;
-
     double old_ratio = ratio;
     ratio = new_ratio;
 
     return old_ratio;
 }
 
-void print_size()
+void print_size(struct State *state)
 {
-    log_info("I think size is: %d", THE_SIZE);
+    log_info("I think size is %d", state->the_size);
 }
