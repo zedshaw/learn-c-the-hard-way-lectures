3619eb56579f3b97e6fe8310aa05d08b9cdaa62c
IammyselfYBX <56785746+IammyselfYBX@users.noreply.github.com>
2022-02-13 20:54:30 +0900

    fix use strtoi

--- ex25/ex25.c
+++ ex25/ex25.c
@@ -1,14 +1,15 @@
-
-#include <stdlib.h>
+/**WARING: This code is fresh and potentially isn't correct yet.*/
+#include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include "dbg.h"
 
 #define MAX_DATA 100
 
-int read_string(char **out_string, int max_buffer)
+int read_string(char **out_string, int max_buffer) 
 {
-    *out_string = calloc(1, max_buffer + 1);
+    *out_string = calloc(1, max_buffer+1);
     check_mem(*out_string);
 
     char *result = fgets(*out_string, max_buffer, stdin);
@@ -17,32 +18,36 @@ int read_string(char **out_string, int max_buffer)
     return 0;
 
 error:
-    if (*out_string) free(*out_string);
+    if(*out_string) free(*out_string);
     *out_string = NULL;
     return -1;
 }
 
-int read_int(int *out_int)
+int read_int(long *out_int) 
 {
     char *input = NULL;
+    char *end = NULL;
     int rc = read_string(&input, MAX_DATA);
-    check(rc == 0, "Failed to read number.");
+    check( 0 == rc, "Failed to read number.");
 
-    *out_int = atoi(input);
+    *out_int = strtoi(input, &end, 10, 0, MAX_DATA, &rc);
+    check( 0 != rc, "Failed to read number.");
+    check((*end == '\0' || *end == '\n') &&
+            *input != '\0', "Invaid number: %s", input);
 
     free(input);
     return 0;
 
 error:
-    if (input) free(input);
+    if(input) free(input);
     return -1;
 }
 
-int read_scan(const char *fmt, ...)
+int read_scan(const char *fmt, ...) 
 {
-    int i = 0;
+    //int i = 0;
     int rc = 0;
-    int *out_int = NULL;
+    long *out_int = NULL;
     char *out_char = NULL;
     char **out_string = NULL;
     int max_buffer = 0;
@@ -50,18 +55,18 @@ int read_scan(const char *fmt, ...)
     va_list argp;
     va_start(argp, fmt);
 
-    for (i = 0; fmt[i] != '\0'; i++) {
-        if (fmt[i] == '%') {
+    for (int i = 0; fmt[i] != '\0'; i++) {
+        if(fmt[i] == '%') {
             i++;
-            switch (fmt[i]) {
+            switch(fmt[i]) {
                 case '\0':
-                    sentinel("Invalid format, you ended with %%.");
+                    sentinel("Invaid format, you ended with %%.");
                     break;
 
                 case 'd':
-                    out_int = va_arg(argp, int *);
+                    out_int = va_arg(argp, long *);
                     rc = read_int(out_int);
-                    check(rc == 0, "Failed to read int.");
+                    check(0==rc, "Failed to read int.");
                     break;
 
                 case 'c':
@@ -73,16 +78,16 @@ int read_scan(const char *fmt, ...)
                     max_buffer = va_arg(argp, int);
                     out_string = va_arg(argp, char **);
                     rc = read_string(out_string, max_buffer);
-                    check(rc == 0, "Failed to read string.");
+                    check(0 == rc, "Failed to read string.");
                     break;
 
                 default:
-                    sentinel("Invalid format.");
+                    sentinel("Invaid format.");
             }
         } else {
             fgetc(stdin);
         }
-
+    
         check(!feof(stdin) && !ferror(stdin), "Input error.");
     }
 
@@ -99,28 +104,29 @@ int main(int argc, char *argv[])
     char *first_name = NULL;
     char initial = ' ';
     char *last_name = NULL;
-    int age = 0;
+    long age = 0;
 
     printf("What's your first name? ");
     int rc = read_scan("%s", MAX_DATA, &first_name);
-    check(rc == 0, "Failed first name.");
+    check(0==rc, "Failed first name.");
 
     printf("What's your initial? ");
-    rc = read_scan("%c\n", &initial);
-    check(rc == 0, "Failed initial.");
+    rc  = read_scan("%c\n", &initial);
+    check(0==rc, "Failed initial.");
 
     printf("What's your last name? ");
     rc = read_scan("%s", MAX_DATA, &last_name);
-    check(rc == 0, "Failed last name.");
+    check(0==rc, "Failed last name.");
 
     printf("How old are you? ");
-    rc = read_scan("%d", &age);
+    rc = read_scan("%d", &age); //这里就是 %d 不要写成 %ld;
+    check(0==rc, "Failed to read age.");
 
-    printf("---- RESULTS ----\n");
+    printf("========== RESULTS ==============\n");
     printf("First Name: %s", first_name);
     printf("Initial: '%c'\n", initial);
     printf("Last Name: %s", last_name);
-    printf("Age: %d\n", age);
+    printf("Age: %ld\n", age);
 
     free(first_name);
     free(last_name);
@@ -128,3 +134,7 @@ int main(int argc, char *argv[])
 error:
     return -1;
 }
+
+
+
+
